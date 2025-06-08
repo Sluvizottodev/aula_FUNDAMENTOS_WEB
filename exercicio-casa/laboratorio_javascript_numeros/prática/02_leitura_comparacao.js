338f15b9a955b948b72e1d4b4752e53aad723169
SLuvizotto <luvizottodev@gmail.com>
2025-06-08 14:41:07 -0300

    feat: adiciona comparação de números com mensagem no console

---
+++ exercicio-casa/laboratorio_javascript_numeros/prática/02_leitura_comparacao.js
@@ -0,0 +1,8 @@
+const num1 = 50;
+const num2 = 20;
+
+console.log (
+    num1 === num2
+        ? `Os números são iguas: ${num1}`
+        : `O número ${num1} é ${num1> num2 ? 'maior' : 'menor'} que ${num2}`
+);
